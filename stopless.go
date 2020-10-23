38934b4a466596a7442f826847a81b070b95f62a
Volodymyr <Volodymyr-pratsin@i.ua>
2020-10-23 12:39:32 +0300

    init commit

---
+++ stopless.go
@@ -0,0 +1,105 @@
+package stopless
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var notifySignals = []os.Signal{
+	syscall.SIGINT,
+	syscall.SIGKILL,
+	syscall.SIGTERM,
+	syscall.SIGQUIT,
+	syscall.SIGHUP,
+}
+
+type Server struct {
+	http.Server
+	signalHooks         map[os.Signal]func()
+	customNotifySignals []os.Signal
+	terminationPeriod   time.Duration
+}
+
+func (srv *Server) ListenAndServe() error {
+	go srv.handleSignals()
+	err := srv.Server.ListenAndServe()
+	if err != nil {
+		if err != http.ErrServerClosed {
+			return err
+		}
+	}
+	return err
+}
+
+func (srv *Server) ListenAndServeTLS(certFile, keyFile string) error {
+	go srv.handleSignals()
+	err := srv.Server.ListenAndServeTLS(certFile, keyFile)
+	if err != nil {
+		if err != http.ErrServerClosed {
+			return err
+		}
+	}
+	return err
+}
+
+func (srv *Server) restart() error {
+	return nil
+}
+
+func (srv *Server) Notify(sig ...os.Signal) {
+	srv.customNotifySignals = append(srv.customNotifySignals, sig...)
+}
+
+func (srv *Server) handleSignals() {
+	var sig os.Signal
+	listener := make(chan os.Signal, 1)
+
+	if len(srv.customNotifySignals) != 0 {
+		notifySignals = srv.customNotifySignals
+	}
+
+	signal.Notify(
+		listener,
+		notifySignals...,
+	)
+
+	for {
+		sig = <-listener
+
+		hook, ok := srv.signalHooks[sig]
+		if ok {
+			hook()
+		}
+
+		switch sig {
+		case syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGINT, syscall.SIGTERM:
+			log.Printf("received %v.", sig)
+			err := srv.shutdown()
+			if err != nil {
+				log.Fatalf("shutdown err: %v\n", err)
+			}
+			break
+		default:
+			log.Printf("received %v: nothing i care about...\n", sig)
+		}
+	}
+}
+
+func (srv *Server) shutdown() error {
+	if srv.terminationPeriod == 0 {
+		srv.terminationPeriod = 30 * time.Second
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), srv.terminationPeriod)
+	defer cancel()
+	err := srv.Server.Shutdown(ctx)
+	if err != nil {
+		return fmt.Errorf("shutdown error: %v\n", err)
+	}
+	return srv.Server.Shutdown(ctx)
+}
